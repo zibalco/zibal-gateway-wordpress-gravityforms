e679d6494513bc1c8cfea4d8b3858ff8d35c045f
mzamanzadeh <mohammad.zamanzadeh@gmail.com>
2019-01-17 20:51:57 +0330

    ipg addon

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه زیبال گرویتی فرم
+Plugin URI: http://gravityforms.ir/
+Description: افزونه درگاه پرداخت زیبال برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.3.0
+Author: گرویتی فرم پارسی
+Author URI: http://gravityforms.ir/
+*/
+if (!defined('ABSPATH')) exit;
+require_once('zibal.php');
