d0d2451f694caab60872203ac4132a0e0406f5a5
mzamanzadeh <mohammad.zamanzadeh@gmail.com>
2019-02-24 15:12:23 +0330

    v1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: درگاه زیبال گرویتی فرم
 Plugin URI: http://gravityforms.ir/
 Description: افزونه درگاه پرداخت زیبال برای فرم ساز فوق پیشرفته Gravity Forms
-Version: 2.3.0
+Version: 2.3.2
 Author: گرویتی فرم پارسی
 Author URI: http://gravityforms.ir/
 */
