604e7e7c69013df7c9c26a24d12b4b4cbbe3886d
YahyaKng <yhy.kng@gmail.com>
2021-06-29 20:19:20 +0430

    Minor fixes

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 Plugin Name: درگاه زیبال گرویتی فرم
 Plugin URI: http://zibal.ir/
 Description: افزونه درگاه پرداخت زیبال برای فرم ساز فوق پیشرفته Gravity Forms
-Version: 1.0.0
-Author: Zibal
+Version: 1.0.1
+Author: Yahya Kangi
 Author URI: http://zibal.ir/
 */
 if (!defined('ABSPATH')) exit;
